0ca9b9179cd9545e622dd50b8c325bdcbf1c6736
Enes <enesaygur387@gmail.com>
2026-05-22 17:12:38 +0300

    create todo list component structure

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import type { Todo } from "./types/todo";
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState("");
+
+  const addTodo = () => {
+    if (!title.trim()) return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: title.trim(),
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setTitle("");
+  };
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Todo App</h1>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Todo yaz..."
+        onKeyDown={(e) => {
+          if (e.key === "Enter") addTodo();
+        }}
+      />
+      <button onClick={addTodo}>Ekle</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
