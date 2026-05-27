fd08b4a581816ab62e61154ca37e5339b1a9c8ab
Enes <enesaygur387@gmail.com>
2026-05-27 16:01:09 +0300

    add localStorage persistence for todos

---
+++ todo-app/src/hooks/useTodos.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import type { Todo } from "./../types/todo";
+
+function useTodos() {
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
+
+  const addTodo = (title: string) => {
+    if (!title.trim()) return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: title.trim(),
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return { todos, addTodo, deleteTodo, toggleTodo };
+}
+
+export default useTodos;
